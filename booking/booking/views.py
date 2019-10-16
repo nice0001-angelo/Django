21785fae184f6e5fc0f7c71c93783bf4e5251b41
nice0001-angelo <nice0001@gmail.com>
2019-10-16 17:52:48 +0900

    update

--- booking/booking/views.py
+++ booking/booking/views.py
@@ -3,14 +3,15 @@ from .models import Booking
 from .serializers import BookingSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 class BookingList(generics.ListCreateAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
 class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication)
+    permission_classes = (IsAuthenticated,IsOwnerOrReadOnly)
 
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
